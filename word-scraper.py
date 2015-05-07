6c638392ffc8dcd3c45558bfe0fdc2e90e2873df
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-05-07 19:23:16 +0530

    OSX Notification API Utilised

--- word-scraper.py
+++ word-scraper.py
@@ -1,12 +1,18 @@
 import requests
+import os
+from random import randint
 from bs4 import BeautifulSoup
 
-count = 0
+word = []
+meaning = []
+
 r = requests.get("https://quizlet.com/58647605/kaplan-900-flash-cards/")
-soup = BeautifulSoup(r.content)
-for word in soup.find_all("div"):
-	id = word.get("data-id") 
-	if id is not None:
-		count = count + 1
-		print id
-print "Count : " + str(count) 
+content = BeautifulSoup(r.content)
+for words in content.findAll('span', attrs={'class':'qWord'}):
+	word.append(words.text.encode("utf-8"))
+
+for word_meanings in content.findAll('span', attrs={'class':'qDef'}):
+	meaning.append(word_meanings.text.encode("utf-8"))
+
+rest_command = """'display notification "{}" with title "{}"'""".format(meaning[randint(0,737)],word[(randint(0,737))])
+os.system("osascript -e "+ rest_command)
