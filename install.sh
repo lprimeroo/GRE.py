19cf60e03b99fabd5cbd154411bfb34f83bba6c4
gunlinux <gunlinux@ya.ru>
2016-10-02 20:54:07 +0800

    Home dir in unix format

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 username=$(whoami)
-mv gre.py /Users/$username/
+mv gre.py ~$username/
 sudo mv com.$username.gre.plist /Library/LaunchAgents
 sudo chmod 600 /Library/LaunchAgents/com.$username.gre.plist 
 sudo chown root /Library/LaunchAgents/com.$username.gre.plist 
-chmod +x /Users/$username/gre.py
+chmod +x ~$username/gre.py
 sudo launchctl load /Library/LaunchAgents/com.$username.gre.plist
